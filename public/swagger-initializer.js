11222edd83643409a4071f33c6cd1bc4cf4c8376
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-11-12 15:10:40 -0300

    Testando linkagem

--- public/swagger-initializer.js
+++ public/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://order-management-orders-swagger.vercel.app/",
+    url: "https://github.com/Gildevan-Pereira/order-management-orders-swagger/blob/master/swagger/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
