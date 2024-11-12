068b4e339351b19a552efb12e6dc07295c1a12a5
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-11-12 16:30:37 -0300

    Ajustando projeto para o pages

--- swagger-ui/dist/swagger-initializer.js
+++ swagger-ui/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://Gildevan-Pereira.github.io/order-management-orders-swagger/swagger.json",
+    url: "https://raw.githubusercontent.com/Gildevan-Pereira/order-management-orders-swagger/refs/heads/master/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
