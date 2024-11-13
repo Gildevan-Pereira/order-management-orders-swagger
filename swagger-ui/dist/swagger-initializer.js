f5e647f06946b8feac93ff07c2083878b838e655
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-11-13 12:27:11 -0300

    Removendo arquivos não utilizados

--- swagger-ui/dist/swagger-initializer.js
+++ swagger-ui/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/Gildevan-Pereira/order-management-orders-swagger/refs/heads/master/swagger.json",
+    url: "https://raw.githubusercontent.com/Gildevan-Pereira/order-management-orders-swagger/refs/heads/master/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
